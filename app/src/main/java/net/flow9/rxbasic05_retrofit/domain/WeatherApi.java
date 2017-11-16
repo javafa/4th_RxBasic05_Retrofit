fa7a65d2b1df9d9f816ce75ed3a2b95a5869be5d
javafa <nigne2016@gmail.com>
2017-11-16 12:03:00 +0900

    Initial commit

---
+++ app/src/main/java/net/flow9/rxbasic05_retrofit/domain/WeatherApi.java
@@ -0,0 +1,22 @@
+package net.flow9.rxbasic05_retrofit.domain;
+
+public class WeatherApi
+{
+    private RealtimeWeatherStation RealtimeWeatherStation;
+
+    public RealtimeWeatherStation getRealtimeWeatherStation ()
+    {
+        return RealtimeWeatherStation;
+    }
+
+    public void setRealtimeWeatherStation (RealtimeWeatherStation RealtimeWeatherStation)
+    {
+        this.RealtimeWeatherStation = RealtimeWeatherStation;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [RealtimeWeatherStation = "+RealtimeWeatherStation+"]";
+    }
+}
